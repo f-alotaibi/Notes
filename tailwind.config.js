9323c0fc89bfa7a457129a4fe2f12cb84937b886
* <121350271+f-alotaibi@users.noreply.github.com>
2024-01-22 22:19:31 +0300

    (pref) minifiy tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './index.html' ],
+  safelist: [
+    // Note elem
+    'flex',
+    'items-center',
+    'justify-between',
+    'px-2',
+    'py-1',
+    'rounded-lg',
+    'hover:bg-yellow-200',
+    'hover:cursor-pointer',
+    // Note elem svg
+    'h-5',
+    'w-5',
+    'text-yellow-500',
+    // Note span elem
+    'text-sm',
+    'text-yellow-900',
+    'select-none',
+    'truncate',
+    // tempnoteelem listener
+    'bg-yellow-200',
+    'hover:cursor-default',
+    'hover:cursor-pointer',
+    //'hover:bg-yellow-200'
+    'italic'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
